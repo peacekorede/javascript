7109d553b9f518efbfdcc5ccc0b4679f43e8bb36
peacekorede <peacevictoria585@gmail.com>
2022-10-18 19:11:44 +0100

    thia is my first javascript tack

---
+++ main.js
@@ -0,0 +1,32 @@
+function capitalization(){
+    const sentence = 'this is a sentence in lowercase structure.';
+    console.log(sentence.toUpperCase());
+
+}
+capitalization()
+
+function changes(){
+    const sentence = 'THIS IS A SENTENCE IN UPPERCASE STRUCTUER.';
+    console.log(sentence.toLowerCase());
+
+}
+changes()
+
+
+function distance(w, x, y, z ){
+    const g = Math.sqrt((w-x)*2 + (y-w)*2)
+    
+    console.log(g) 
+}
+distance(84, 53,447,125)
+
+
+function stringfromstring(){
+   
+}
+const message = "taking a string from a string";
+const result = message.slice(9, 29);
+console.log(result);  
+
+
+
